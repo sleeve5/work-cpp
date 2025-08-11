d89c37c609e5eeaa68c27371435a31ee1f7aef30
sleeve5 <1244595156@qq.com>
2025-08-11 17:27:57 +0800

    first upload

---
+++ 01arr.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int arr[10];
+    
+    // 静态数组定义，赋值
+    // memset(arr, 0, sizeof(arr));
+    // arr[0] = 1;
+    // arr[1] = 2;
+    // int a = arr[0];
+    // cout << a << endl;
+
+    // 增-尾加
+    // for(int i = 0; i < 10; i++){
+    //     arr[i] = i;
+    // }
+    // arr[4] = 4;
+    // arr[5] = 5;
+
+    // 增-插入
+    // for(int i = 4; i > 2; i--){
+    //     arr[i] = arr[i-1];
+    // }
+    // arr[2] = 666;
+
+    // 增-内存空间满
+    // int newArr[20];
+    // memset(newArr, 0, sizeof(newArr));
+    // for(int i = 0; i < 10; i++){
+    //     newArr[i] = arr[i];
+    // }
+    // newArr[10] = 666;
+
+    // newArr[10] = -1;    // -1 means delete
+
+    // 删
+    // for(int i = 0; i < 5; i++){
+    //     arr[i] = i;
+    // }
+
+    // for(int i = 1; i < 5; i++){
+    //     arr[i] = arr[i+1];
+    // }
+    // arr[4] = -1;
+    
+    // 动态数组：动态数组底层还是静态数组，只是自动帮我们进行数组空间的扩缩容，并把增删查改操作进行了封装，让我们使用起来更方便而已。
+    vector<int> arr;
+
+    
+    cout << arr[4] << endl;
+    
+    return 0;
+}
--- base.cpp
+++ base.cpp
@@ -1,1 +1,37 @@
-#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    // standard output
+    // cout << a << endl;
+    // cout << "Hello" << "," << "World!" << endl;
+    // string s = "abc";
+    // cout << s << " " << a << endl;
+
+    // control flow
+    // if (a > 5) {
+    //     cout << "a > 5" << endl;
+    // } else if (a == 5) {
+    //     cout << "a == 5" << endl;
+    // } else {
+    //     cout << "a < 5" << endl;
+    // }
+
+    // loop
+    for (int i = 0; i <5; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+    int num = 100;
+    while (num > 0) {
+        cout << num << " ";
+        num /= 2;
+    }
+
+    // data structures
+    
+
+    return 0;
+}
+

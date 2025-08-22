ef0b6e74b66ad655bc5e2077b16946a29cb661d1
sleeve5 <1244595156@qq.com>
2025-08-22 18:54:31 +0800

    queue

--- 00base.cpp
+++ 00base.cpp
@@ -36,17 +36,18 @@ int main()
     // // 动态数组底层还是静态数组，只是自动帮我们进行数组空间的扩缩容，并把增删查改操作进行了封装，让我们使用起来更方便而已。是尾进尾出（LIFO）的 随机访问序列容器
     // vector<int> v1;                 // 各种定义方式
     // intVec v2;
-    // vector<int> v3(8);
-    // vector<int> v4{1, 2, 3, 4};
+    // vector<int> v3(6);              // 长度，全0
+    // vector<int> v4{1, 2, 3, 4};     // 定义
     // intVec v5(v4);                  // 拷贝构造
     // intVec v6(move(v4));            // 移动构造
 
     // vector<int> arr;
-    // for (int i = 0; i < 10; i++) {
+    // for (int i = 0; i < 6; i++) {
     //     arr.push_back(i);               // 尾进
     // }
     // arr.resize(5);                      // 改元素数量size
-    // cout << arr.capacity();             // 打印16，每次指数级翻倍
+    // cout << arr.capacity() << endl;     // 打印容量16，每次指数级翻倍
+    // cout << arr.size() << endl;         // 打印长度
     // arr.insert(arr.begin() + 2, 666);   // 指定位置插入
     // arr.insert(arr.begin(), -1);
     // arr.pop_back();                     // 尾出
@@ -57,17 +58,29 @@ int main()
     // arr.reserve(10);                    // 改容量capacity
     // arr.clear();                        // 清空所有元素
 
-    //  C++ 标准库提供的 哈希表（Hash Table） 容器，用来存放 键-值对（key-value pairs），无序（遍历顺序与插入顺序无关）。平均常数时间 O(1) 的插入、查找、删除（最坏退化到 O(n)）。
-    unordered_map<string, int> map;
-    map["apple"] = 5;
-    map.emplace("banana", 3);
-    cout << map.at("apple") << endl;
-    cout << map.count("banana") << endl;
-    cout << map.size() << endl;
-    for (auto& [k, v] : map) {
-        cout << k << ":" << v << "  ";
-    }
-    map.clear();
+    // //  C++ 标准库提供的 哈希表（Hash Table） 容器，用来存放 键-值对（key-value pairs），无序（遍历顺序与插入顺序无关）。平均常数时间 O(1) 的插入、查找、删除（最坏退化到 O(n)）。
+    // unordered_map<string, int> map;
+    // map["apple"] = 5;
+    // map.emplace("banana", 3);
+    // cout << map.at("apple") << endl;
+    // cout << map.count("banana") << endl;
+    // cout << map.size() << endl;
+    // for (auto& [k, v] : map) {
+    //     cout << k << ":" << v << "  ";
+    // }
+    // map.clear();
 
+    // deque是一个 “先进先出（FIFO）” 的容器，底层默认用 std::deque，只暴露 “队尾进、队头出” 的接口。平均O(1)
+    queue<int> q;
+    for (int i = 0; i < 5; i++) {
+        q.push(i);                      // 尾进
+    }
+    q.pop();                            // 头出
+    cout << q.size() << endl;           // 长度
+    while (!q.empty()) {
+        cout << q.front() << "  ";
+        q.pop();
+    }
+    
     return 0;
 }

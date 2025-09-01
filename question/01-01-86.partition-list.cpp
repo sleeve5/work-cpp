cddaa7eea381d86e756ecfbffd585b3d4d0f49b4
sleeve5 <1244595156@qq.com>
2025-09-01 08:43:24 +0800

    partition list

---
+++ question/01-01-86.partition-list.cpp
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=86 lang=cpp
+ * @lcpr version=30202
+ *
+ * [86] 分隔链表
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+};
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        
+    }
+};
+// @lc code=end
+
+
+
+
+/*
+// @lcpr case=start
+// [1,4,3,2,5,2]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,1]\n2\n
+// @lcpr case=end
+
+ */
+

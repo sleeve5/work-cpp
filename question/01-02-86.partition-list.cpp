74aa813acb983ac97c2c81b87902466e7ef16a95
sleeve5 <1244595156@qq.com>
2025-09-01 09:31:29 +0800

    86. 分隔链表

---
+++ question/01-02-86.partition-list.cpp
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=86 lang=cpp
+ * @lcpr version=30202
+ *
+ * [86] 分隔链表
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+};
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* list1 = new ListNode(0);
+        ListNode* list2 = new ListNode(0);
+        ListNode* p1 = list1;
+        ListNode* p2 = list2;
+        while (head != nullptr) {
+            if (head->val < x) {
+                p1->next = head;
+                p1 = p1->next;
+            } else {
+                p2->next = head;
+                p2 = p2->next;
+            }
+            ListNode* tmp = head;
+            head = head->next;
+            tmp->next = nullptr;
+        }
+        p1->next = list2->next;
+        return list1->next;
+    }
+};
+// @lc code=end
+
+
+
+
+/*
+// @lcpr case=start
+// [1,4,3,2,5,2]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,1]\n2\n
+// @lcpr case=end
+
+ */
+

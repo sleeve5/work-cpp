a3ce54d7cb108aa1512c802e6ecb67252b91c461
sleeve5 <1244595156@qq.com>
2025-09-10 16:24:09 +0800

    160. 相交链表

---
+++ question/01-07-160.intersection-of-two-linked-lists.cpp
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=cpp
+ * @lcpr version=30203
+ *
+ * [160] 相交链表
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+};
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* p1 = headA;
+        ListNode* p2 = headB;
+        while (p1 != p2) {
+            if (p1 == nullptr) {
+                p1 = headB;
+            } else {
+                p1 = p1->next;
+            }
+            if (p2 == nullptr) {
+                p2 = headA;
+            } else {
+                p2 = p2->next;
+            }
+        }
+        return p1;
+    }
+};
+// @lc code=end
+
+
+
+
+/*
+// @lcpr case=start
+// 8\n[4,1,8,4,5]\n[5,6,1,8,4,5]\n2\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 2\n[1,9,1,2,4]\n[3,2,4]\n3\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 0\n[2,6,4]\n[1,5]\n3\n2\n
+// @lcpr case=end
+
+ */
+

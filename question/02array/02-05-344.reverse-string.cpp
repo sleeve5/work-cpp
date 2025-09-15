0003b9029c71b0663748388871af3843558204d8
sleeve5 <1244595156@qq.com>
2025-09-15 17:08:07 +0800

    334. 反转字符串

---
+++ question/02array/02-05-344.reverse-string.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=cpp
+ * @lcpr version=30203
+ *
+ * [344] 反转字符串
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+};
+
+// @lc code=start
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+};
+// @lc code=end
+
+
+
+
+/*
+// @lcpr case=start
+// ["h","e","l","l","o"]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// ["H","a","n","n","a","h"]\n
+// @lcpr case=end
+
+ */
+

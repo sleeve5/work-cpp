c1d3294e0530cdaa60e28520ad57aaa3e34c54ad
sleeve5 <1244595156@qq.com>
2025-09-15 20:08:07 +0800

    701. 二分查找

---
+++ tech/binarysearch/01-704.binary-search.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=cpp
+ * @lcpr version=30203
+ *
+ * [704] 二分查找
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+};
+
+// @lc code=start
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
+
+
+
+/*
+// @lcpr case=start
+// [-1,0,3,5,9,12]\n9\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [-1,0,3,5,9,12]\n2\n
+// @lcpr case=end
+
+ */
+

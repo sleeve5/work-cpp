8a1eddee257a6eb3f1c4f4790a57ddb0d29a4dc9
sleeve5 <1244595156@qq.com>
2025-09-15 17:00:03 +0800

    167. 两数之和 II - 输入有序数组

---
+++ question/02array/02-04-167.two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=cpp
+ * @lcpr version=30203
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+};
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0;
+        int right = numbers.size() - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return {left + 1, right + 1};
+            } else if (sum > target) {
+                right--;
+            } else if (sum < target) {
+                left++;
+            }
+        }
+        return {-1, -1};
+    }
+};
+// @lc code=end
+
+
+
+
+/*
+// @lcpr case=start
+// [2,7,11,15]\n9\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,3,4]\n6\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [-1,0]\n-1\n
+// @lcpr case=end
+
+ */
+

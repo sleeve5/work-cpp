446c6f23f63dbaa26b5e76e2a1de8d9d2fd4f6a8
sleeve5 <1244595156@qq.com>
2025-09-16 15:05:55 +0800

    206. 反转链表

---
+++ tech/traverse/02-206.reverse-linked-list.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ * @lcpr version=30203
+ *
+ * [206] 反转链表
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+};
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         if (!head || !head->next) {
+//             return head;
+//         }
+//         ListNode* cur = head;
+//         ListNode* pre = nullptr;
+//         while (cur) {
+//             ListNode* next = cur->next;
+//             cur->next = pre;
+//             pre = cur;
+//             cur = next;
+//         }
+//         return pre;
+//     }
+// };
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+        ListNode* last = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return last;
+    }
+};
+// @lc code=end
+
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
+

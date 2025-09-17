d2dae80a9c6e490250f954c6c93a7cca2e435fa0
sleeve5 <1244595156@qq.com>
2025-09-17 14:52:39 +0800

    80. 删除有序数组中的重复项 Ⅱ

---
+++ 125.valid-palindrome.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=125 lang=cpp
+ * @lcpr version=30203
+ *
+ * [125] 验证回文串
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+};
+
+// @lc code=start
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+    }
+};
+// @lc code=end
+
+
+
+
+/*
+// @lcpr case=start
+// "A man, a plan, a canal: Panama"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "race a car"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// " "\n
+// @lcpr case=end
+
+ */
+

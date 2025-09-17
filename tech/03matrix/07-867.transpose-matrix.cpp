acd57b9d0764353aed4571dad04d5de155067f9a
sleeve5 <1244595156@qq.com>
2025-09-17 20:48:47 +0800

    867. 转置矩阵

---
+++ tech/03matrix/07-867.transpose-matrix.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=867 lang=cpp
+ * @lcpr version=30203
+ *
+ * [867] 转置矩阵
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+};
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> res(n, vector<int>(m));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
+
+
+
+/*
+// @lcpr case=start
+// [[1,2,3],[4,5,6],[7,8,9]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[1,2,3],[4,5,6]]\n
+// @lcpr case=end
+
+ */
+

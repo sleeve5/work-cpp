6271cbc1abbc3ab5444d89a318f6c33c096f3635
sleeve5 <1244595156@qq.com>
2025-09-17 10:53:26 +0800

    48. 旋转图像

---
+++ tech/03matrix/02-48.rotate-image.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=cpp
+ * @lcpr version=30203
+ *
+ * [48] 旋转图像
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+};
+
+// @lc code=start
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for (int i = 0; i < matrix[0].size(); i++) {
+            for (int j = i; j < matrix[i].size(); j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        for (auto& row : matrix) {
+            reverse(row.begin(), row.end());
+        }
+    }
+};
+// @lc code=end
+
+
+
+
+/*
+// @lcpr case=start
+// [[1,2,3],[4,5,6],[7,8,9]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]\n
+// @lcpr case=end
+
+ */
+

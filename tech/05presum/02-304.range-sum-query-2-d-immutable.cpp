23c9fa2bfdbc5587c95e75a1f2a449973ce6e23e
sleeve5 <1244595156@qq.com>
2025-09-25 16:35:49 +0800

    304. 二维区域和检索 - 矩阵不可变

---
+++ tech/05presum/02-304.range-sum-query-2-d-immutable.cpp
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=304 lang=cpp
+ * @lcpr version=30203
+ *
+ * [304] 二维区域和检索 - 矩阵不可变
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+};
+
+// @lc code=start
+class NumMatrix {
+public:
+    vector<vector<int>> presum;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        if (!(m || n)) {
+            return;
+        }
+        presum.resize(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                presum[i][j] = presum[i - 1][j] + presum[i][j - 1] - presum[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return presum[row2 + 1][col2 + 1] - presum[row2 + 1][col1] - presum[row1][col2 + 1] + presum[row1][col1];
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
+// @lc code=end
+
+
+
+
+/*
+// @lcpr case=start
+// ["NumMatrix","sumRegion","sumRegion","sumRegion"]\n[[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]\n
+// @lcpr case=end
+
+ */
+

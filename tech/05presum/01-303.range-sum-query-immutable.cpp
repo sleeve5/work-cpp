ae7a4acf17045715d8e3574f14d16c25b8e1adbb
sleeve5 <1244595156@qq.com>
2025-09-25 16:10:53 +0800

    303. 区域和检索 - 数组不可变

---
+++ tech/05presum/01-303.range-sum-query-immutable.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=cpp
+ * @lcpr version=30203
+ *
+ * [303] 区域和检索 - 数组不可变
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+};
+
+// @lc code=start
+class NumArray {
+    std::vector<int> preSum;
+
+    public:
+    NumArray(std::vector<int>& nums) {
+        preSum.resize(nums.size() + 1);
+        for (int i = 1; i < preSum.size(); i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
+        }
+    }
+
+    int sumRange(int left, int right) {
+        return preSum[right + 1] - preSum[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
+// @lc code=end
+
+
+
+
+/*
+// @lcpr case=start
+// ["NumArray", "sumRange", "sumRange", "sumRange"]\n[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]\n
+// @lcpr case=end
+
+ */
+

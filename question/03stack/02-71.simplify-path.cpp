ed5c035adc0778a7702f10375b1b5519b08624a8
sleeve5 <1244595156@qq.com>
2025-09-29 16:04:45 +0800

    71. 简化路径

---
+++ question/03stack/02-71.simplify-path.cpp
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=71 lang=cpp
+ * @lcpr version=30203
+ *
+ * [71] 简化路径
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+};
+
+// @lc code=start
+class Solution {
+public:
+    string simplifyPath(string path) {
+        istringstream ss(path);
+        vector<string> parts;
+        string part;
+        while (getline(ss, part, '/')) {
+            if (part.empty() || part == ".") {
+                continue;
+            }
+            if (part == "..") {
+                if (!parts.empty()) {
+                    parts.pop_back();
+                }
+            } else {
+                parts.push_back(part);
+            }
+        }
+        string res;
+        for (auto p : parts) {
+            res += "/" + p;
+        }
+        return res.empty() ? "/" : res;
+    }
+};
+// @lc code=end
+
+
+
+
+/*
+// @lcpr case=start
+// "/home/"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "/home//foo/"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "/home/user/Documents/../Pictures"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "/../"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "/.../a/../b/c/../d/./"\n
+// @lcpr case=end
+
+ */
+

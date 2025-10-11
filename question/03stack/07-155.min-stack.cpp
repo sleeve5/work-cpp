e57b412536b92cd43b0341b983291c0874fe3b2f
sleeve5 <1244595156@qq.com>
2025-10-11 10:32:11 +0800

    155. 最小栈

--- question/03stack/07-155.min-stack.cpp
+++ question/03stack/07-155.min-stack.cpp
@@ -29,25 +29,33 @@ public:
 
 // @lc code=start
 class MinStack {
+    stack<int> stk;
+    stack<int> minStk;
 public:
     MinStack() {
         
     }
     
     void push(int val) {
-        
+        stk.push(val);
+        if (minStk.empty() || val <= minStk.top()) {
+            minStk.push(val);
+        }
     }
     
     void pop() {
-        
+        if (stk.top() == minStk.top()) {
+            minStk.pop();
+        }
+        stk.pop();
     }
     
     int top() {
-        
+        return stk.top();
     }
     
     int getMin() {
-        
+        return minStk.top();
     }
 };
 
@@ -60,12 +68,6 @@ public:
  * int param_4 = obj->getMin();
  */
 // @lc code=end
-
-int main() {
-    MinStack sl;
-
-}
-
 
 
 /*
